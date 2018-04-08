d71db3453cdc22d629a515eee62d6ebcb968504e
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2018-04-08 19:25:21 +0200

    Initial front & back integration

---
+++ client/containers/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import io from 'socket.io-client';
+
+import styles from './App.css';
+
+import MessageForm from '../components/MessageForm';
+import MessageList from '../components/MessageList';
+import UsersList from '../components/UsersList';
+import UserForm from '../components/UserForm';
+
+const socket = io('/');
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            messages: [],
+            text: '',
+            name: '',
+        };
+    }
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({ users }) => this.chatUpdate(users));
+    }
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages }); //equal to this.setState({messages:messages});
+    }
+    chatUpdate(users) {
+        this.setState({ users });
+    }
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages });
+        socket.emit('message', message);
+    }
+    handleUserSubmit(name) {
+        this.setState({ name });
+        socket.emit('join', name);
+    }
+    renderUserForm() {
+        return <UserForm onUserSubmit={name => this.handleUserSubmit(name)} />;
+    }
+    renderLayout() {
+        return (
+            <div className={styles.App}>
+                <div className={styles.AppHeader}>
+                    <div className={styles.AppTitle}>ChatApp</div>
+                    <div className={styles.AppRoom}>App room</div>
+                </div>
+                <div className={styles.AppBody}>
+                    <UsersList users={this.state.users} />
+                    <div className={styles.MessageWrapper}>
+                        <MessageList messages={this.state.messages} />
+                        <MessageForm
+                            onMessageSubmit={message =>
+                                this.handleMessageSubmit(message)
+                            }
+                            name={this.state.name}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    render() {
+        return this.state.name !== ''
+            ? this.renderLayout()
+            : this.renderUserForm();
+    }
+}
+
+export default hot(module)(App);
---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const UserService = require('./UserService.js');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const userService = new UserService();
+
+app.use('/', express.static(path.join(__dirname, '../build')));
+
+io.on('connection', function(socket) {
+    socket.on('join', function(name) {
+        userService.addUser({
+            id: socket.id,
+            name,
+        });
+        io.emit('update', {
+            //czemu io zamiast socket?
+            users: userService.getAllUsers(),
+        });
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('disconnect', () => {
+        userService.removeUser(socket.id);
+        socket.broadcast.emit('update', {
+            users: userService.getAllUsers(),
+        });
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('message', function(message) {
+        const { name } = userService.getUserById(socket.id);
+        socket.broadcast.emit('message', {
+            text: message.text,
+            from: name,
+        });
+    });
+});
+
+server.listen(3000, function() {
+    console.log('listening on *:3000');
+});
